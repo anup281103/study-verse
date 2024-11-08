4bde3f25d712905621919fb66c7db2fd0a1dc7ae
anup281103 <anup281103@gmail.com>
2024-11-09 01:10:53 +0530

    image removed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
-import loginImg from "../assets/Images/login.webp"
-import Template from "../components/core/Auth/Template"
+import loginImg from "../assets/Images/login.webp";
+import Template from "../components/core/Auth/Template";
 
 function Login() {
   return (
@@ -7,10 +7,10 @@ function Login() {
       title="Welcome Back"
       description1="Build skills for today, tomorrow, and beyond."
       description2="Education to future-proof your career."
-      image={loginImg}
+      // image={loginImg}
       formType="login"
     />
-  )
+  );
 }
 
-export default Login
+export default Login;
